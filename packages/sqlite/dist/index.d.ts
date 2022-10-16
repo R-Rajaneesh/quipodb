33d014438a651979165308b34d8e9d0186a8cfb7
R-Rajaneesh <rajaneeshr2006@gmail.com>
2022-10-16 17:27:01 +0530

    feat: @quipodb/sqlite

---
+++ packages/sqlite/dist/index.d.ts
@@ -0,0 +1,25 @@
+import sqlite from "better-sqlite3";
+interface sqliteConstructor {
+    path: String;
+    primaryKey: any;
+}
+interface document {
+    [key: string]: string | number | Object | Array<any> | any;
+}
+export default class Sqlite {
+    sqlite: sqlite.Database;
+    private options;
+    private collectionName;
+    primaryKey: any;
+    constructor(options: sqliteConstructor);
+    createCollectionProvider(collectionName: String, cb?: Function): void;
+    private createColumnProvider;
+    createDocProvider(data: document, cb?: Function): any;
+    deleteCollectionProvider(collectionName: String, cb?: Function): void;
+    deleteDocProvider(data: document, cb?: Function): void;
+    getCollectionProvider(collectionName: String, cb?: Function): any[];
+    getDocProvider(data: document, cb?: Function): any;
+    getCollectionsProvider(cb?: Function): any[];
+    updateDocProvider(refData: document, data: document, cb?: Function): void;
+}
+export {};
